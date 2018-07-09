7338f7c4530fc557cd7b8d8a5f4c0d0ae7eaece3
Arthur Jen <arthurljen@gmail.com>
2018-07-08 18:56:04 -0700

    added ls -lah

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,13 +1,14 @@
+const exec = require('child_process').exec;
+
 function start() {
     console.log('Request handler "start" was called.');
-    function sleep(milliseconds) {
-        const startTime = new Date().getTime();
-        while(new Date().getTime() < startTime + milliseconds);
+    const content = 'empty';
 
-    }
+    exec('ls -lah', function(error, stdout, stderr) {
+        content = stdout;
+    });
 
-    sleep(10000);
-    return 'Hello Start';
+    return content;
 }
 
 function upload() {
