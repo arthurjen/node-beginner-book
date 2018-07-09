636bb7bd25b264c4be248c51e3441e9b14df88a8
Arthur Jen <arthurljen@gmail.com>
2018-07-08 19:00:37 -0700

    added response to router

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,11 +1,12 @@
 const exec = require('child_process').exec;
 
-function start() {
+function start(response) {
     console.log('Request handler "start" was called.');
-    const content = 'empty';
 
     exec('ls -lah', function(error, stdout, stderr) {
-        content = stdout;
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.write(stdout);
+        response.end();
     });
 
     return content;
@@ -13,7 +14,9 @@ function start() {
 
 function upload() {
     console.log('Request handler "upload" was called.');
-    return 'Hello upload';
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write('Hello Upload');
+    response.end();
 }
 
 exports.start = start;
