6d48ccf844ad294a613158fc7526057bbf0b5679
Arthur Jen <arthurljen@gmail.com>
2018-07-08 17:24:14 -0700

    added url

---
+++ server.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const url = require('url');
+
+function start() {
+    function onRequest(req, res) {
+        const pathname = url.parse(req.url).pathname;
+        console.log(`Request for ${pathname} received.`);
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.write('Hello World');
+        res.end();
+    }
+
+    http.createServer(onRequest).listen(8888);
+    console.log('Server has started.');
+}
+
+exports.start = start;
