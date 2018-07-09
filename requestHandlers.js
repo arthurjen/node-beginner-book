3c496b44361fa580656e004d1b8665b250a9705f
Arthur Jen <arthurljen@gmail.com>
2018-07-08 18:18:48 -0700

    added returns for handlers and sleep for start handler

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,9 +1,18 @@
 function start() {
     console.log('Request handler "start" was called.');
+    function sleep(milliseconds) {
+        const startTime = new Date().getTime();
+        while(new Date().getTime() < startTime + milliseconds);
+
+    }
+
+    sleep(10000);
+    return 'Hello Start';
 }
 
 function upload() {
     console.log('Request handler "upload" was called.');
+    return 'Hello upload';
 }
 
 exports.start = start;
